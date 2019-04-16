6d1d03c7b0c9c347e86bb85bbbe8b63a4e634881
github username <github email address>
2019-04-16 14:58:02 -0400

    split page into two divs

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,13 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :username, :games
+
+  def games
+    self.object.games.map do |game|
+        {score: game.score,
+        longest_word: game.longest_word,
+        time: game.time}
+    end
+
+  end
+
+end
