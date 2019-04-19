7117d2ca2d9f00695b6b578409810dcc8fac1a0e
github username <github email address>
2019-04-19 11:58:13 -0400

    unlimited cors api request to our url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://wordswithnerds.herokuapp.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
